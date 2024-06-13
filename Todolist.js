f6bb598fbdb54cc4cabc75d0228e839cc1e5a634
Vikashartist <111743731+Vikashartist@users.noreply.github.com>
2024-06-13 14:07:11 +0530

    Create Todolist.js

---
+++ Todolist.js
@@ -0,0 +1,33 @@
+
+
+  const inpItem = document.getElementById('inputitems');
+  const todobox = document.getElementById('to-do-box');
+  inpItem.addEventListener("keyup",function(event){
+      if(event.key=="Enter"&&this.value!=""){
+      
+       addtodo(this.value);
+       this.value = "";
+      }
+    });
+    
+    const addtodo = (inpItem) =>{
+      
+      
+      const listItem = document.createElement("li");
+      listItem.innerHTML=`${inpItem}<i class="fa-solid fa-xmark" style="color: #ffffff;"></i>`;
+     listItem.addEventListener("click",function(){
+       this.classList.toggle("done");
+     });
+     listItem.addEventListener("click",function(){
+       listItem.remove();
+       console.log("remove was clicked"); 
+     });
+     
+    // listItem.querySelector("i").addEventListener{
+    //   "click",function(){
+    //     listItem.remove();
+    //   }
+    // }
+     todobox.appendChild(listItem); 
+    }
+   
